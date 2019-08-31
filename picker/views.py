fadbda2daa9ab0b7a58ce1cde433d96b98b0d543
ydhnwb <yudhanewbie@gmail.com>
2019-08-31 18:07:28 +0700

    fixed url GET

--- picker/views.py
+++ picker/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 import tweepy
 import random
+from django.shortcuts import redirect
 
 
 def init_api():
@@ -77,11 +78,6 @@ def result(request):
                 return HttpResponse(template.render(context, request))
 
     else:
-        variable = "get"
-        template = loader.get_template("picker/result.html")
-        context = {
-            "data": variable
-        }
-        return HttpResponse(template.render(context, request))
+        return redirect("index")
 
 
